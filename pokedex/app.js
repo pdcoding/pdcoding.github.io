8d5f622ab9f2cd9d185827d5429694b2f7d6f438
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-07-16 18:43:35 -0400

    EOD commit

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,13 +1,40 @@
 console.log("Gotta catch 'em all!");
 
-$(() => {
-  const pokemonSearch = 'cubone';
-  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
+const pokemonSearch = 'charizard';
+const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
 
+// const typeSearch = 'flying';
+// const endpoint = `https://pokeapi.co/api/v2/${typeSearch}/`;
+
+$(() => {
   //   console.log(endpoint);
 
-  const data = $.ajax({
+  //   const data = $.ajax({
+  //     url: endpoint
+  //   });
+
+  const handleData = data => {
+    console.log(data.types[0].type.name);
+    // console.log(data.type);
+
+    const $name = $('<h2>').text(data.name);
+    $('.container').append($name);
+    $name.addClass('pokemonName');
+
+    const $type = $('<h2>').text(data.type);
+    $('.container').append($type);
+    $name.addClass('pokemonType');
+  };
+
+  $.ajax({
     url: endpoint
-  });
-  console.log(data);
+  }).then(handleData);
+  // const
 });
+
+//name -> name
+//pokemon # -> id
+//types -> types[""0""].type types[0].type.name
+//weight - in kg -> weight
+//height - in cm -> height
+//sprites -> sprites.front_default
