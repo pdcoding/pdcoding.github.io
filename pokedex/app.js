8dc49e836809925dede0392853adcc163546d2c5
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-07-16 11:53:10 -0400

    now the files are actually linked correctly

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,1 +1,8 @@
 console.log("Gotta catch 'em all!");
+
+$(() => {
+  const pokemonSearch = 'cubone';
+  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
+
+  console.log(endpoint);
+});
