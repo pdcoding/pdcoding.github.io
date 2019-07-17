5043acef868078e4815a92fd8ed04d54a2bcb7a6
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-07-17 12:10:18 -0400

    can pull specific data and made the page not refresh

--- pokedex/app.js
+++ pokedex/app.js
@@ -1,40 +1,62 @@
 console.log("Gotta catch 'em all!");
 
-const pokemonSearch = 'charizard';
-const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
+$('form').on('submit', event => {
+  //   event.preventDefault();
 
-// const typeSearch = 'flying';
-// const endpoint = `https://pokeapi.co/api/v2/${typeSearch}/`;
+  const pokemonSearch = $('.pokemonSearch').val();
+  const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
 
-$(() => {
-  //   console.log(endpoint);
+  const textField = $(() => {
+    const handleData = data => {
+      // console.log(data.game_indices[0].game_index);
 
-  //   const data = $.ajax({
-  //     url: endpoint
-  //   });
+      //NAME
+      const $name = $('<h2>').text('Name: ' + data.name);
+      $('.container').append($name);
+      $name.addClass('pokemonName');
 
-  const handleData = data => {
-    console.log(data.types[0].type.name);
-    // console.log(data.type);
+      //Poké#
+      const $pokeNum = $('<h2>').text(
+        'Pokémon #' + data.game_indices[0].game_index
+      );
+      $('.container').append($pokeNum);
+      $pokeNum.addClass('pokeNum');
 
-    const $name = $('<h2>').text(data.name);
-    $('.container').append($name);
-    $name.addClass('pokemonName');
+      //TYPE
+      const $type =
+        $('<h2>').text('Type: ' + data.types[0].type.name) +
+        ' / ' +
+        types[1].type.name;
+      $('.container').append($type);
+      $type.addClass('pokemonType');
 
-    const $type = $('<h2>').text(data.type);
-    $('.container').append($type);
-    $name.addClass('pokemonType');
-  };
+      //Weight
+      const $weight = $('<h2>').text('Weight: ' + data.weight + ' hectograms');
+      $('.container').append($weight);
+      $type.addClass('weight');
+
+      //Height
+      const $height = $('<h2>').text('Height: ' + data.height + ' decimeters');
+      $('.container').append($height);
+      $height.addClass('height');
+
+      //Sprite
+      const $sprite = $('<img>');
+      $sprite.attr('src', data.sprites.front_default);
+      $('.container').append($sprite);
+      $sprite.addClass('sprite');
+      $sprite.attr('id', 'sprite');
+    };
+  });
 
   $.ajax({
     url: endpoint
   }).then(handleData);
-  // const
 });
 
 //name -> name
 //pokemon # -> id
 //types -> types[""0""].type types[0].type.name
-//weight - in kg -> weight
-//height - in cm -> height
+//weight - in hectograms -> weight
+//height - in decimeters -> height
 //sprites -> sprites.front_default
