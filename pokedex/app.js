c3f0dd0696ae0e26d185f68f332b4f230176f113
Peter Dilenschneider <peterdilenschneider@Peters-MacBook-Pro-2.local>
2019-07-17 15:17:31 -0400

    created another div for the search results

--- pokedex/app.js
+++ pokedex/app.js
@@ -6,46 +6,43 @@ $(() => {
 
     //NAME
     const $name = $('<h2>').text('Name: ' + data.name);
-    $('.container').append($name);
+    $('.results').append($name);
     $name.addClass('pokemonName');
 
     //Poké#
-    const $pokeNum = $('<h2>').text(
-      'Pokémon #' + data.game_indices[0].game_index
-    );
-    $('.container').append($pokeNum);
+    const $pokeNum = $('<h2>').text('Pokémon #' + data.id);
+    $('.results').append($pokeNum);
     $pokeNum.addClass('pokeNum');
 
     //TYPE
     const $type = $('<h2>').text(
       'Type: ' + data.types[0].type.name
-    ); /*+
-      ' / ' +
-      types[1].type.name*/
-    $('.container').append($type);
+    ); /* + ' / ' + data.types[1].type.name
+    );*/
+    $('.results').append($type);
     $type.addClass('pokemonType');
 
     //Weight
     const $weight = $('<h2>').text('Weight: ' + data.weight + ' hectograms');
-    $('.container').append($weight);
+    $('.results').append($weight);
     $type.addClass('weight');
 
     //Height
     const $height = $('<h2>').text('Height: ' + data.height + ' decimeters');
-    $('.container').append($height);
+    $('.results').append($height);
     $height.addClass('height');
 
     //Sprite
     const $sprite = $('<img>');
     $sprite.attr('src', data.sprites.front_default);
-    $('.container').append($sprite);
+    $('.results').append($sprite);
     $sprite.addClass('sprite');
     $sprite.attr('id', 'sprite');
   };
 
   $('form').on('submit', event => {
+    $('.results').empty();
     event.preventDefault();
-    console.log('test submit');
 
     const pokemonSearch = $('.pokemonSearch').val();
     const endpoint = `https://pokeapi.co/api/v2/pokemon/${pokemonSearch}/`;
